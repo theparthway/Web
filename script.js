97c36f76510cfe66cba46e794c66b9b8b8b7f02b
theparthway <parthpsh@gmail.com>
2021-06-06 00:44:01 +0530

    Bug fixes

---
+++ script.js
@@ -0,0 +1,5 @@
+const btnclock = document.querySelector(".clock");
+const btncolorflipper = document.querySelector(".color-flipper");
+const btncool = document.querySelector(".cool");
+const btntodolist = document.querySelector(".todo-list");
+
